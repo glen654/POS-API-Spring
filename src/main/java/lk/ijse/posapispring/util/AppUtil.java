9be3cd1ddc6bd18cb9fd04cd23013c7b62fe57ef
Glen Alloy <glenalloy8@gmail.com>
2024-10-06 12:08:12 +0530

    created AppUtil class for generating ids

---
+++ src/main/java/lk/ijse/posapispring/util/AppUtil.java
@@ -0,0 +1,20 @@
+package lk.ijse.posapispring.util;
+
+import java.util.UUID;
+
+public class AppUtil {
+    public static String generateUserId(){
+        return "USER-" + UUID.randomUUID();
+    }
+    public static String generateCustomerId(){
+        return "CUS-" + UUID.randomUUID();
+    }
+
+    public static String generateItemId(){
+        return "ITEM-" + UUID.randomUUID();
+    }
+
+    public static String generateOrderId(){
+        return "ORDER-" + UUID.randomUUID();
+    }
+}
