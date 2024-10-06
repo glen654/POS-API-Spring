24ca527c37054b8271f23217ca9467f02a82c9e6
Glen Alloy <glenalloy8@gmail.com>
2024-10-06 19:48:25 +0530

    implemented entity classes for the customer and item

--- src/main/java/lk/ijse/posapispring/entity/impl/CustomerEntity.java
+++ src/main/java/lk/ijse/posapispring/entity/impl/CustomerEntity.java
@@ -1,6 +1,22 @@
 package lk.ijse.posapispring.entity.impl;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lk.ijse.posapispring.entity.SuperEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "customer")
 public class CustomerEntity implements SuperEntity {
+    @Id
+    private String customerId;
+    private String customerName;
+    private String customerAddress;
+    private String customerTel;
 }
--- src/main/java/lk/ijse/posapispring/entity/impl/ItemEntity.java
+++ src/main/java/lk/ijse/posapispring/entity/impl/ItemEntity.java
@@ -1,4 +1,22 @@
 package lk.ijse.posapispring.entity.impl;
 
-public class ItemEntity {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lk.ijse.posapispring.entity.SuperEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "item")
+public class ItemEntity implements SuperEntity {
+    @Id
+    private String itemCode;
+    private String itemName;
+    private int qtyOnHand;
+    private double unitPrice;
 }
